966aabca665059b119dbe4dabf66b6d7aeac87a1
Rae Stanton <raestanton93@gmail.com>
2023-08-01 10:03:24 -0700

    scopey scope

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = "some value";
+  leastFavoriteCustomer = "new value";
+}
